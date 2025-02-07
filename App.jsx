a4a62590a21cadfdc05477cd5bfe9d029e4e373d
sahi755 <sahilbhosale162@gmail.com>
2025-02-07 14:09:16 +0530

    get commit

---
+++ App.jsx
@@ -0,0 +1,26 @@
+
+function App() {
+
+  let name="Sahil";
+  const age=25;
+  name="kunal";
+  
+  function add(m1,m2){
+
+      return m1+m2;
+  }
+
+  return (
+    <>
+      <div>
+
+       <h3>my name{name} and my {age}</h3>
+       <h3>sum:{add(2,3)}</h3>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
